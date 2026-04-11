3d653694726c05e49e31b78ff7a0b67b0922eceb
KhadzA <alimkhadz111@gmail.com>
2026-04-11 23:54:17 +0800

    Duplicate

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,7 +17,6 @@ function App() {
     <Routes>
 
       <Route path="/" element={<Home />} />
-      <Route path="/auth/login" element={<Login />} />
       <Route
         path="/auth/login"
         element={
